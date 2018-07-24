3db7bf9bd6e324cd3a920e11989151cb7986fc9d
Sun Lintong <sunlintong@ghostcloud.cn>
2018-07-24 15:20:39 +0800

    Delete tenant.sh

--- tenant.sh
+++
@@ -1,6 +0,0 @@
-name=`kubectl get pod -n ekos-plugin | grep ekos-tenant-server | awk '{print $1}'`
-docker load -i tenant.tar
-rm tenant.tar
-docker push registry.ekos.local/ekos/tenant
-kubectl delete pod $name -n ekos-plugin
-kubectl get pod -n ekos-plugin | grep tenant
