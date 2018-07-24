e65e24827c0ecf0bc96de4e42bf9a800e729c5ad
Sun Lintong <sunlintong@ghostcloud.cn>
2018-07-24 15:14:18 +0800

    Update save.sh

--- save.sh
+++ save.sh
@@ -1,4 +1,16 @@
-rm tenant.tar
+#!/bin/bash
+
+if [ $# = 0 ]; then
+    name="paas"
+else
+    name=$1
+fi
+
+cd $GOPATH/src/git.ghostcloud.cn/ekos/ekos-plugin-$name
 rm -rf bin
-docker tag registry.newben.net/dev/tenant registry.ekos.local/ekos/tenant
-docker save registry.ekos.local/ekos/tenant -o tenant.tar
+rm $name.tar
+
+docker tag registry.newben.net/dev/$name registry.ekos.local/ekos/$name
+docker save registry.ekos.local/ekos/$name -o $name.tar
+docker rmi registry.newben.net/dev/$name
+docker rmi registry.ekos.local/ekos/$name
