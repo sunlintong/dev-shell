e33cfc08f9480cc1de103ad84f0900c6dd8adbe7
Sun Lintong <sunlintong@ghostcloud.cn>
2018-07-24 15:09:58 +0800

    Delete monitor.sh

--- monitor.sh
+++
@@ -1,6 +0,0 @@
-name=`kubectl get pod -n ekos-plugin | grep ekos-monitor-server | awk '{print $1}'`
-docker load -i monitor.tar
-rm monitor.tar
-docker push registry.ekos.local/ekos/monitor
-kubectl delete pod $name -n ekos-plugin
-kubectl get pod -n ekos-plugin | grep monitor
