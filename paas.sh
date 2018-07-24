b995a0b1fb25c3a699274f03b84885d428c43341
Sun Lintong <sunlintong@ghostcloud.cn>
2018-07-24 15:11:55 +0800

    Delete paas.sh

--- paas.sh
+++
@@ -1,6 +0,0 @@
-name=`kubectl get pod -n ekos-plugin | grep ekos-paas-server | awk '{print $1}'`
-docker load -i paas.tar
-rm paas.tar
-docker push registry.ekos.local/ekos/paas
-kubectl delete pod $name -n ekos-plugin
-kubectl get pod -n ekos-plugin | grep paas
