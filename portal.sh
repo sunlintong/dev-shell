afc9baa89586a466fc15cbacaad2d77f7e2c37f4
Sun Lintong <sunlintong@ghostcloud.cn>
2018-07-24 15:12:49 +0800

    Delete portal.sh

--- portal.sh
+++
@@ -1,6 +0,0 @@
-name=`kubectl get pod -n kube-system | grep ekos-portal-server | awk '{print $1}'`
-docker load -i portal.tar
-rm portal.tar
-docker push registry.ekos.local/ekos/portal
-kubectl delete pod $name -n kube-system
-kubectl get pod -n kube-system | grep portal
